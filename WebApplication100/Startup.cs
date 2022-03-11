63baf87536c23e2005d1f45d2719d696e137ff6e
Navya Thallapally <navyathallapally4@gmail.com>
2022-03-11 15:37:32 +0530

    Hi Pavani

--- WebApplication100/Startup.cs
+++ WebApplication100/Startup.cs
@@ -33,6 +33,7 @@ namespace WebApplication100
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hi Pavani");
                 });
             });
         }
